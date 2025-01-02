31ebd59fe649898c7b15bde45f3763c5cfdd3757
Shivang Kashyap Ankur <91110618+ShivangkashyapAnkur@users.noreply.github.com>
2025-01-02 11:35:37 +0530

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,17 +1,1 @@
-// Define two numbers
-let num1 = 10;
-let num2 = 5;
 
-// Perform calculations
-let sum = num1 + num2;
-let difference = num1 - num2;
-let product = num1 * num2;
-let quotient = num1 / num2;
-
-// Display results
-// console.log("Sum: " + sum);
-// console.log("Difference: " + difference);
-// console.log("Product: " + product);
-// console.log("Quotient: " + quotient);
-// console.log("2" < 1);
-// console.log(1 <"2");
